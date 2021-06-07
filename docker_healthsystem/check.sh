40f64679e2d458343c34201f214b274c4993a447
XHunter <kartikey@koders.in>
2021-06-07 22:55:44 +0530

    ⚡️ Removed sudo usage - Rootless mode

--- docker_healthsystem/check.sh
+++ docker_healthsystem/check.sh
@@ -15,7 +15,7 @@ ret_error() {
 	fi
 
 	### Name seperation
-	tmp=$(sudo docker inspect --format="{{.Name}}" "$1")
+	tmp=$(docker inspect --format="{{.Name}}" "$1")
 	IFS='/' read -ra tmp <<< "$tmp"
 
 	name=""
@@ -24,10 +24,10 @@ ret_error() {
 		name="${name} ${i}"
 	done
 
-	msg="𝗖𝗼𝗻𝘁𝗮𝗶𝗻𝗲𝗿 𝗜𝗗 : $1\n𝗖𝗿𝗲𝗮𝘁𝗲𝗱 𝗔𝘁 : $(date -d $(sudo docker inspect --format='{{.State.StartedAt}}' "$1"))"
+	msg="𝗖𝗼𝗻𝘁𝗮𝗶𝗻𝗲𝗿 𝗜𝗗 : $1\n𝗖𝗿𝗲𝗮𝘁𝗲𝗱 𝗔𝘁 : $(date -d $(docker inspect --format='{{.State.StartedAt}}' "$1"))"
 	if [[ ! $2 == "true" ]]
 	then
-	msg="$msg\n𝗙𝗮𝗶𝗹𝗲𝗱 𝗔𝘁 : $(date -d $(sudo docker inspect --format='{{.State.FinishedAt}}' "$1"))\n𝗘𝘅𝗶𝘁 𝗖𝗼𝗱𝗲 : $(sudo docker inspect --format='{{.State.ExitCode}}' "$1")\n"
+	msg="$msg\n𝗙𝗮𝗶𝗹𝗲𝗱 𝗔𝘁 : $(date -d $(docker inspect --format='{{.State.FinishedAt}}' "$1"))\n𝗘𝘅𝗶𝘁 𝗖𝗼𝗱𝗲 : $(docker inspect --format='{{.State.ExitCode}}' "$1")\n"
 	fi
 
 	fmsg="{ \"wait\": true, \"embeds\": [{ \"_\": \"_\", \"title\": \"${name}\", \"description\": \"\", \"color\": \"16711680\", \"timestamp\": \"$(date -u --iso-8601=seconds)\", \"author\": { \"_\": \"_\", \"name\":\"Docker Report\", \"icon_url\": \"https://imgur.com/axp9PKK.png\"}, \"thumbnail\": {  }, \"image\": { \"_\": \"_\" }, \"footer\": { \"_\":\"_\" } }], \"embeds\": [{ \"_\": \"_\", \"title\": \"\", \"description\": \"${msg}\", \"color\":\"${color}\", \"timestamp\": \"$(date -u --iso-8601=seconds)\", \"author\": { \"_\": \"_\", \"name\":\"${name}\", \"icon_url\": \"https://imgur.com/axp9PKK.png\"}, \"thumbnail\": {  }, \"image\": { \"_\": \"_\" }, \"footer\": { \"_\":\"_\", \"text\": \"Made with ❤️  by Koders\"} }] }"
@@ -35,9 +35,9 @@ ret_error() {
 	curl -H "Content-Type: application/json" -H "Expect: application/json" -X POST "${webhook}" -d "${fmsg}" 2>/dev/null
 }
 
-sudo docker ps -aq > LOG.log
+docker ps -aq > LOG.log
 
 while IFS= read -r line
 do
-	ret_error "${line}" "$(sudo docker inspect "${line}" --format='{{.State.Running}}')"
+	ret_error "${line}" "$(docker inspect "${line}" --format='{{.State.Running}}')"
 done < LOG.log
