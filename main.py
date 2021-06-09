46d5f41a67e9a9c2ed4a7267df2e1c75d890823d
DakshYadav358 <61241857+DakshYadav358@users.noreply.github.com>
2021-06-09 14:39:31 +0530

    Added conatainer for fast forward functionalinty

--- main.py
+++ main.py
@@ -42,6 +42,45 @@ bot = commands.Bot(command_prefix="~")
 async def on_ready():  # Triggers when bot is ready
     logger.warning("Kourage is running at version {0}".format(CONFIG.VERSION))
 
+@bot.command()
+@commands.has_any_role("@everyone")
+async def ff(msg):
+    """
+    This module fast forwards the messages from one channel to another
+    Usage: prefix + `ff` + number_of_message =< 10 + channel_name
+    Return: None
+    """
+    last_n_messages=int(msg.message.content.split(' ')[1])+1
+    max_messages=100
+    if last_n_messages-1 > max_messages:
+        await msg.channel.send(f"maximum masseges limit to be forwarded is {max_messages} you have entered {last_n_messages-1}")
+        return
+
+    message_details=await msg.channel.history(limit=int(last_n_messages)).flatten() 
+    channel_name=msg.message.content.split(' ')[2]
+    channel_name=channel_name[2:(len(channel_name)-1)]
+    channel=bot.get_channel(int(channel_name))
+    print(message_details[1].id)
+    if channel is not None:
+        loop_itterator=1
+        message_to_be_sent_id=[]
+        message_to_be_forwarded=[]
+        
+           
+        while loop_itterator<last_n_messages:
+            if message_details[loop_itterator].attachments:
+                image=message_details[loop_itterator].attachments[0]
+                await channel.send(image)
+            message_to_be_sent_id.append((message_details[loop_itterator]).id)
+            loop_itterator=loop_itterator+1   
+        for each_id in message_to_be_sent_id:
+                message_to_be_forwarded.append((await msg.fetch_message(each_id)).content)
+        message_to_be_forwarded.reverse()
+        for each in message_to_be_forwarded:
+            await channel.send(each)
+    else:
+        await msg.channel.send('Channel name not found')
+
 if __name__ == "__main__":
     try:
         bot.run(CONFIG.TOKEN)
