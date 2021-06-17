50436371e39c264aae0986ce2a22daed53d6e6ee
SUMYAK JAIN <3456gaurav@gmail.com>
2021-06-17 17:25:01 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -17,4 +17,4 @@ run_cmd() {
 
 git_branch=$(git branch 2>/dev/null | grep '^*' | colrm 1 2 | tr -d '\n' && echo  -n)
 run_cmd "docker build -t ${git_branch} ." "Docker file built."
-run_cmd "docker run -e TOKEN ${git_branch}" "Run"
+run_cmd "docker run -e TOKEN -e ADMIN_CHANNEL_ID ${git_branch}" "Run"
